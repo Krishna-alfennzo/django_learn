75cf0c1419ef46d56bd2f5a1bc5c8be6913e319c
Krishna-alfennzo <krishna@alfennzo.in>
2026-08-11 18:17:41 +0530

    django commit

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.index)
+    path("<str:month>", views.monthly_challenges),
+    path("<int:month>", views.monthly_challenges_by_numbers)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": None
+}
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the challenges index.")
+def monthly_challenges_by_numbers(request, month):
+    return HttpResponse(month)
+
+
+def monthly_challenges(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+  
